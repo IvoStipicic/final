3ab704f140c80f63806e35de8dfceef28aa3701d
IvoStipicic <62021857+IvoStipicic@users.noreply.github.com>
2021-07-13 20:59:23 -0300

    final

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,15 @@
-  
 <template>
   <div class="final">
     <input type="text" :value="input" @input="actualizar($event)" /> <br />
     <h4>cantidad: {{ input.length }}</h4>
     <h4>{{ first | codificar }}</h4>
-    <h4>{{ second | mayuscula }}</h4>
-    <h4>{{ third | minuscula }} </h4>
-    <h4>{{ four | mix1 }} </h4>
-    <h4>{{ five | mix2 }}</h4>
-    <br><hr>
-    <p>Respuestas: 1:b, 2:b , 3:c, 4:b, 5:b,c </p>
-    
+    <h4>{{ second | uppercase }}</h4>
+    <h4>{{ third | lowercase }}</h4>
+    <h4>{{ four | mezclando1 }}</h4>
+    <h4>{{ five | mezclando2 }}</h4>
+    <br />
+    <hr />
+    <p>Respuestas: 1:b, 2:b , 3:c, 4:b, 5:b,c</p>
   </div>
 </template>
 
@@ -40,43 +39,43 @@ export default {
   computed: {},
   filters: {
     codificar: function(value) {
-      let palabraCodificada = '';
-      let palabraMinuscula = value.toLowerCase();
+      let palabraAModificar = "";
+      let palabralowercase = value.toLowerCase();
       for (let i = 0; i < String(value).length; i++) {
-        let letra = palabraMinuscula[i];
+        let letra = palabralowercase[i];
         switch (letra) {
           case "a":
-            letra = "u"
+            letra = "u";
             break;
           case "e":
-            letra = "o"
+            letra = "o";
             break;
           case "o":
-            letra = "e"
+            letra = "e";
             break;
           case "u":
-            letra = "a"
+            letra = "a";
             break;
         }
-        palabraCodificada += letra;
+        palabraAModificar += letra;
       }
-      return palabraCodificada;
+      return palabraAModificar;
     },
-    mayuscula: function(value) {
+    uppercase: function(value) {
       if (!value) return "";
       value = value.toString();
       return value.toUpperCase();
     },
-    minuscula: function(value) {
+    lowercase: function(value) {
       if (!value) return "";
       value = value.toString();
       return value.toLowerCase();
     },
-    mix1: function (value) {
+    mezclando1: function(value) {
       value = String(value);
-      var flip = '';
+      var flip = "";
       for (var i = 0; i < String(value).length; i++) {
-        if(i % 2 === 0){
+        if (i % 2 === 0) {
           flip += value.charAt(i).toUpperCase();
         } else {
           flip += value.charAt(i).toLowerCase();
@@ -84,11 +83,11 @@ export default {
       }
       return flip;
     },
-    mix2: function (value) {
+    mezclando2: function(value) {
       value = String(value);
-      var flip = '';
+      var flip = "";
       for (var i = 0; i < String(value).length; i++) {
-        if(i % 2 !== 0){
+        if (i % 2 !== 0) {
           flip += value.charAt(i).toUpperCase();
         } else {
           flip += value.charAt(i).toLowerCase();
@@ -96,7 +95,6 @@ export default {
       }
       return flip;
     },
-    
   },
 };
 </script>
